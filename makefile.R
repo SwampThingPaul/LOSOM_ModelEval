cca921560c400ed8f4bd7fd130fedeb34c7659b2
Paul Julian <36565183+SwampThingPaul@users.noreply.github.com>
2021-04-05 11:54:02 -0400

    initial commit!

---
+++ makefile.R
@@ -0,0 +1,10 @@
+
+## makefile.R
+### knits and moves files to folder for online sharing
+
+list.files(full.names=T)
+files=c("./libs/","./Plots/","./SCCF_iter1_LOSOM.html","./SCCF Logo knockout_c.png","./ConSWFL.jpeg","./SCCF_iter1_LOSOM_ENLMrslt.Rmd")
+
+webpage.loc="c:/Julian_LaCie/_GitHub/sccf-tech.github.io/slides/LOSOM"
+# Folder.Maker(paste0(webpage.loc,"/iter1/"))
+file.copy(files,paste0(webpage.loc,"/iter1"),overwrite=T,recursive=T)
