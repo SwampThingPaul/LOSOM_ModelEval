3a534a473da6ac4cc9dbb95359be37d70affec18
Paul Julian <36565183+SwampThingPaul@users.noreply.github.com>
2021-04-08 11:48:54 -0400

    edits/additions

--- makefile.R
+++ makefile.R
@@ -8,3 +8,7 @@ files=c("./libs/","./Plots/","./SCCF_iter1_LOSOM.html","./SCCF_iter1_LOSOM_ENLMr
 webpage.loc="c:/Julian_LaCie/_GitHub/sccf-tech.github.io/slides/LOSOM"
 # Folder.Maker(paste0(webpage.loc,"/iter1/"))
 file.copy(files,paste0(webpage.loc,"/iter1"),overwrite=T,recursive=T)
+
+files=c("./DataReports/Iteration1_stagecurve.html","./DataReports/footer.html")
+webpage.loc="c:/Julian_LaCie/_GitHub/sccf-tech.github.io/DataReports/LOSOM"
+file.copy(files,webpage.loc,overwrite=T,recursive=T)
